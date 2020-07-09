d0a8bda7ee12740a5f6dd145798a89c06a39fe30
Yingqi <chanwk18@hotmail.com>
2020-07-08 23:49:34 -0600

    clean up unnecessary code

--- inhouse-marketing/src/components/seo.js
+++ inhouse-marketing/src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate= "Inhouse Orders - Custom Online Ordering Sites"
       meta={[
         {
           name: `description`,
