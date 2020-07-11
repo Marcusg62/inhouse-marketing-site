c2ca10966a808a24dd378c8ce6b248da36007ea7
Yingqi <chanwk18@hotmail.com>
2020-07-11 17:31:56 -0600

    set up basic render function in signUp

--- inhouse-marketing/src/pages/signUp.js
+++ inhouse-marketing/src/pages/signUp.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 import FormFormat from "../components/Formformat";
 // import "../components/style/signUp.css"
 
-export default class signUp {
+export default class signUp extends React.Component {
   state = {
     step: 1,
     restaurantName: "",
@@ -11,6 +11,43 @@ export default class signUp {
     name: "",
     phone:""  
   }
+
+  // process to next step
+  next = function(){
+    const {step} = this.state
+    this.setState({
+      step: step+1
+    })
+  }
+  
+  // process to previous step
+  back = function(){
+    const {step} = this.state
+    this.setState({
+      step: step-1
+    })
+  }
+
+  handleChange = input => e => {
+    this.setState({
+      [input]: e.target.value
+    })
+  }
+  
+  render(){
+    const { step } = this.state
+    const { restaurantName, restaurantAddress, name, phone } = this.state
+    const values = { restaurantName, restaurantAddress, name, phone }
+
+    switch (step) {
+      case 1:
+        return <h1>step 1</h1>
+
+      case 2:
+        return <h1>step 2</h1>
+    }
+  }
+
 }
 
 
