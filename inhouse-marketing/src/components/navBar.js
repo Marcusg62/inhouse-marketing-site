c916d3e8d8b9d0c25011f2d8d5e0402e19645da6
Yingqi <chanwk18@hotmail.com>
2020-07-14 13:54:45 -0600

    set appBar style

--- inhouse-marketing/src/components/navBar.js
+++ inhouse-marketing/src/components/navBar.js
@@ -3,39 +3,38 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { grey } from '@material-ui/core/colors';
+import MenuItem from "@material-ui/core/MenuItem"
+import { indigo } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    backgroundColor: grey
+    backgroundColor: "#e8eaf6",
+    color: "black"
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 1
   },
 }));
 
-export default function ButtonAppBar() {
+export default function NavBar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h7" className={classes.title}>
-            Inhouse Orders
-          </Typography>
-          <IconButton edge="false" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        <Typography variant="h5" className={classes.title}>
+          Inhouse Orders
+        </Typography>
+        <IconButton edge="false" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 }
