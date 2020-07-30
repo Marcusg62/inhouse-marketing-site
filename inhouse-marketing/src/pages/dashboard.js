517b0fc32455b08380956f77f35b4bf00991e21a
Yingqi <chanwk18@hotmail.com>
2020-07-30 05:35:55 -0600

    render dashboard when not from onboarding form

--- inhouse-marketing/src/pages/dashboard.js
+++ inhouse-marketing/src/pages/dashboard.js
@@ -9,14 +9,21 @@ const Dashboard = props => {
           2. if there is no user, detect the state that if it comes from the submitting site,
           if so, add a paragraph saying congrats on registration
           3. if no user and not come from a submitted page, just ask for sign in or sign up
-           */}   
+           */} 
+    const fromOnboardingForm = () => {
+        if(props.location.state){
+            return props.location.state.fromOnboardingForm? true:false
+        }
+    }  
+
+
     const renderDashboard = () =>{
        return (
-//          <>
-// {           props.location.state.fromOnboardingForm? 
-//            <h1>You submit succesfully! Let's create a user account here.</h1> : null}
-//            {createUserForm()}
-//          </>
+         <>
+          {fromOnboardingForm()? 
+           <h1>You submit succesfully! Let's create a user account here.</h1> : null}
+           {createUserForm()}
+         </>
          
        )
     }
