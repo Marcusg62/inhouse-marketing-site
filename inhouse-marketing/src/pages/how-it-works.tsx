e313f97dcb8fcea64541b94757839e0f47c50193
Marcus Gallegos <marcus.gallegos@ucdenver.edu>
2020-08-12 12:53:25 -0600

    Added GA

--- inhouse-marketing/src/pages/how-it-works.tsx
+++ inhouse-marketing/src/pages/how-it-works.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import Timeline from "../components/timeline"
 import { Divider, Box, Card, CardContent } from "@material-ui/core"
+import ContactForm from "../components/contactForm"
 
 const process = () => {
   const renderProcess = () => {
@@ -16,32 +17,27 @@ const process = () => {
             <Timeline></Timeline>
           </Box>
           <Box width="100%" maxWidth="400px" pt="1rem">
-            <Card>
-              <CardContent>
-                <h1>TO DO</h1>
-                <p>Place 'Get Started' Component Here</p>
-              </CardContent>
-            </Card>
+            <ContactForm></ContactForm>
           </Box>
         </Box>
 
         <h2 className="m-t-20 fs-36">FAQ - Frequently Asked Questions</h2>
         <hr className="" />
         <div
-          itemscope
-          itemprop="mainEntity"
-          itemtype="https://schema.org/Question"
+          itemScope
+          itemProp="mainEntity"
+          itemType="https://schema.org/Question"
           className="p-l-16 m-t-16"
         >
-          <h3 itemprop="name" className="fs-22">
+          <h3 itemProp="name" className="fs-22">
             How do I get paid?{" "}
           </h3>
           <div
-            itemscope
-            itemprop="acceptedAnswer"
-            itemtype="https://schema.org/Answer"
+            itemScope
+            itemProp="acceptedAnswer"
+            itemType="https://schema.org/Answer"
           >
-            <div itemprop="text">
+            <div itemProp="text">
               <p>
                 When customers order online, you will receive funds directly
                 from Stripe into your bank account. Be sure that your Bank
@@ -53,20 +49,20 @@ const process = () => {
         </div>
 
         <div
-          itemscope
-          itemprop="mainEntity"
-          itemtype="https://schema.org/Question"
+          itemScope
+          itemProp="mainEntity"
+          itemType="https://schema.org/Question"
           className="p-l-16 m-t-16"
         >
-          <h3 itemprop="name" className="fs-22">
+          <h3 itemProp="name" className="fs-22">
             What happens to my domain name?{" "}
           </h3>
           <div
-            itemscope
-            itemprop="acceptedAnswer"
-            itemtype="https://schema.org/Answer"
+            itemScope
+            itemProp="acceptedAnswer"
+            itemType="https://schema.org/Answer"
           >
-            <div itemprop="text">
+            <div itemProp="text">
               <p>
                 If you already have a website, we will transfer your domain to
                 our registrar so that we can update your website. We'll take
@@ -78,20 +74,20 @@ const process = () => {
         </div>
 
         <div
-          itemscope
-          itemprop="mainEntity"
-          itemtype="https://schema.org/Question"
+          itemScope
+          itemProp="mainEntity"
+          itemType="https://schema.org/Question"
           className="p-l-16 m-t-16"
         >
-          <h3 itemprop="name" className="fs-22">
+          <h3 itemProp="name" className="fs-22">
             How do I contact support?{" "}
           </h3>
           <div
-            itemscope
-            itemprop="acceptedAnswer"
-            itemtype="https://schema.org/Answer"
+            itemScope
+            itemProp="acceptedAnswer"
+            itemType="https://schema.org/Answer"
           >
-            <div itemprop="text">
+            <div itemProp="text">
               <p>
                 We will match you with a Inhouse Representative who speaks
                 English. If you experience any technical difficulties, you can
--- inhouse-marketing/src/pages/pricing.tsx
+++ inhouse-marketing/src/pages/pricing.tsx
@@ -1,7 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import "../components/style/pricing.css"
-import { Card, Box, CardContent, Divider, CardMedia } from "@material-ui/core"
+import {
+  Card,
+  Box,
+  CardContent,
+  Divider,
+  CardMedia,
+  Grid,
+} from "@material-ui/core"
 import StoreMallDirectory from "@material-ui/icons/StoreMallDirectory"
 import LocationOn from "@material-ui/icons/LocationOn"
 import AccountBalance from "@material-ui/icons/AccountBalance"
@@ -12,41 +19,38 @@ const pricing = () => {
     <Layout>
       <Box m="2rem" textAlign="center" mt="10rem">
         <h1 className="m-b-16 txt-normal">
-          Pricing that works for your restaurant
+          We keep your customers coming back. 
         </h1>
-        <p>
-          We want to keep customers coming back. That's why we have a low fee of
-          5%. We also let you choose who pays the fee.{" "}
+        <p style={{maxWidth: "600px", margin: "auto"}}>
+          We take a low fee of 5% on the Subtotal + Tax. You keep 100% of the tips. You choose who pays the fee. We reccomend eating the fee to be keep customers coming back. {" "}
         </p>
       </Box>
-      <Box p="2rem" display="flex" justifyContent="center" flexWrap="wrap">
-        <Box minWidth={100} width="100%" maxWidth={200} px="5px">
-          <Card variant="outlined">
-            <CardContent className="txt-center">
-              <p className="m-b-16">Saver</p>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                mx="auto"
-                style={{
-                  width: "5rem",
-                  height: "5rem",
-                  border: "2px solid #ddd",
-                  borderRadius: "50%",
-                }}
-              >
-                <LocationOn style={{ fontSize: "3rem" }} />
-              </Box>
-              <h3 className="txt-lighter m-t-16">FREE</h3>
-              <p className="txt-lighter m-t-16">
-                This is good for restaurants who don't want to pay fees. The
-                customer pays a 5% fee added to the total.{" "}
-              </p>
-            </CardContent>
-          </Card>
-        </Box>
-        <Box minWidth={100} width="100%" maxWidth={200} px="5px">
+      <Grid container spacing={2} style={{maxWidth: "900px", margin: "auto"}}>
+        <Grid item xs={12} md={4}>
+          <div className="txt-center p-16">
+            <p className="m-b-16">Saver</p>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              mx="auto"
+              style={{
+                width: "5rem", 
+                height: "5rem",
+                border: "2px solid #ddd",
+                borderRadius: "50%",
+              }}
+            >
+              <LocationOn style={{ fontSize: "3rem" }} />
+            </Box>
+            <h3 className="txt-lighter m-t-16">FREE</h3>
+            <p className="txt-lighter m-t-16">
+              This is good for restaurants who don't want to pay fees. The
+              customer pays a 5% fee added to the total.{" "}
+            </p>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={4}>
           <Card variant="outlined">
             <CardContent className="txt-center">
               <p className="m-b-16">Reccomended</p>
@@ -66,83 +70,74 @@ const pricing = () => {
               </Box>
               <h3 className="txt-lighter m-t-16">5%</h3>
               <p className="txt-lighter m-t-16">
-                This is good for restaurants who want to beat other platforms
-                who charge higher fees and have customers keep coming back.{" "}
-              </p>
-              <p>Restaurants who choose this option get 25% more orders*</p>
-            </CardContent>
-          </Card>
-        </Box>
-        <Box minWidth={100} width="100%" maxWidth={200} px="5px">
-          <Card variant="outlined">
-            <CardContent className="txt-center">
-              <p className="m-b-16">Restaurateur</p>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                mx="auto"
-                style={{
-                  width: "5rem",
-                  height: "5rem",
-                  border: "2px solid #ddd",
-                  borderRadius: "50%",
-                }}
-              >
-                <AccountBalance style={{ fontSize: "3rem" }} />
-              </Box>
-              <h3 className="txt-lighter m-t-16">Contact Us</h3>
-              <p className="txt-lighter m-t-16">
-                For restaurantuers or franchisees with multiple locations who
-                want a completely custom ordering platform.{" "}
-              </p>
-              <p>
-                With high ordering volume, we can offer a monthly per-location
-                subscription. Contact us for more details
+                The strategic choice - easy to beat other platforms
+                who charge higher fees. {" "}
               </p>
             </CardContent>
           </Card>
-        </Box>
-      </Box>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <div className="txt-center p-16">
+            <p className="m-b-16">Restaurateur</p>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              mx="auto"
+              style={{
+                width: "5rem",
+                height: "5rem",
+                border: "2px solid #ddd",
+                borderRadius: "50%",
+              }}
+            >
+              <AccountBalance style={{ fontSize: "3rem" }} />
+            </Box>
+            <h3 className="txt-lighter m-t-16">Contact Us</h3>
+            <p className="txt-lighter m-t-16">
+              For restaurantuers or franchisees with multiple locations who want
+              a completely custom ordering platform.{" "}
+            </p>
+            <p>
+              With high ordering volume, we can offer a monthly per-location
+              subscription. Contact us for more details
+            </p>
+          </div>
+        </Grid>
+      </Grid>
       <Box m="2rem" textAlign="center">
         <Box width="100%" maxWidth={700} mx="auto">
           <Card className="w-100 m-b-16">
-            <CardContent>
-              <h1 className="m-b-16 txt-normal">What's included?</h1>
-              <p className="m-b-16">
-                No matter which option you choose, you get the following
-                features and services.{" "}
-              </p>
-              <h3 >Online Ordering</h3>
-              <p>Contactless Ordering & Payment</p>
-              <p>Daily Payouts</p>
-              
-
+            <CardContent className="txt-left">
+              <h1 className="m-b-16 txt-normal txt-center">What's included?</h1>
+              <h3>Online Ordering</h3>
+              <li>Contactless Ordering & Payment</li>
+              <li>Daily Payouts</li> 
+ 
               <h3 className="m-t-16">Free Website & Domain</h3>
-              <p>SEO (Search Engine Optimization)</p>
-              <p>Mobile Friendly</p>
-              <p>No annual renewal fees</p>
-              <p>Branded and customizable</p>
-              <p>Free SSL certificate</p>
+              <li>SEO (Search Engine Optimization)</li>
+              <li>Mobile Friendly</li>
+              <li>No annual renewal fees</li>
+              <li>Branded and customizable</li>
+              <li>Free SSL certificate</li>
 
-              <h3 className="m-t-16">Google Business Account</h3>
-              <p>Free Account Verification on Google</p>
-              <p>Update & Maintain Hours, Prices, etc</p>
-              <p>Top Ranking On Google</p>
+              <h3 className="m-t-16">Online Presence</h3>
+              <li>Free Account Verification on Google, Yelp, etc. </li>
+              <li>Update & Maintain Hours, Prices, etc</li>
+              <li>Top Ranking On Google</li>
 
-              <h3 className="m-t-16">Account Advisor</h3>
-              <p>
+              <h3 className="m-t-16">Account Specialist</h3>
+              <li>
                 We'll match you with an advisor who speaks your language and
                 knows the industry.
-              </p>
-              <p>24/7 access to your advisor</p>
-              <p>No outsourced support in other countries</p>
+              </li>
+              <li>24/7 access to your advisor</li>
+              <li>No outsourced support in other countries</li>
 
-              <h3 className="m-t-16">On-Site Setup</h3>
-              <p>Free On-Site Setup and Consultation</p>
-              <p>Receipt Printing Setup*</p>
-              <p></p>
-            </CardContent>
+              <h3 className="m-t-16">On-Site Support</h3>
+              <li>Free On-Site Setup and Consultation</li>
+              <li>Receipt Printing Setup (upon printer purchase)</li>
+            </CardContent> 
           </Card>
         </Box>
       </Box>{" "}
@@ -166,7 +161,7 @@ const pricing = () => {
 
           <Card className="w-100 m-b-16">
             <CardContent>
-              <h3>Marketing</h3>
+              <h3>Printed Marketing</h3>
               <p>Starting at $25</p>
               <p>
                 So you have online ordering, but do your customers know about
@@ -179,10 +174,11 @@ const pricing = () => {
             <CardContent>
               <h3>Receipt Printer</h3>
               <p>$250* price may vary</p>
-              <p>Out reccomended printing solution. </p>
+              <p>Reccomended printing solution. </p>
             </CardContent>
           </Card>
         </Box>
+        <p>*Ask a rep for more details </p>
       </Box>{" "}
     </Layout>
   )
