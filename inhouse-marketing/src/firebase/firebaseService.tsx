35d07b257d32a1c839caf3f2e1b2f09e56f1fc45
Marcus Gallegos <marcus.gallegos@ucdenver.edu>
2020-08-13 13:25:13 -0600

    Suggested fix

--- inhouse-marketing/src/firebase/firebaseService.tsx
+++ inhouse-marketing/src/firebase/firebaseService.tsx
@@ -1,27 +1,30 @@
 //This document will handle everything to connect with firebase database or authetication
 import firebase from "gatsby-plugin-firebase"
 
-const googleProvider = new firebase.auth.GoogleAuthProvider()
-const facebookProvider = new firebase.auth.FacebookAuthProvider()
-
-const auth = firebase.auth()
-const db = firebase.firestore()
 const firebaseFunctions = firebase.functions()
 
-export const fetchUserRestaurant = async (userEmail) => {
- let result = await db.collection("onboarding_form_users").doc(userEmail).get()
-                      .then(doc => {
-                        if (doc.exists) {
-                            console.log("Document data:", doc.data());
-                        } else {
-                            // doc.data() will be undefined in this case
-                            console.log("No such document!");
-                        }})
- console.log(result)
- return result
+export const fetchUserRestaurant = async userEmail => {
+  const db = firebase.firestore()
+
+  let result = await db
+    .collection("onboarding_form_users")
+    .doc(userEmail)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data())
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!")
+      }
+    })
+  console.log(result)
+  return result
 }
 
 const creditialsWithPopup = provider => {
+  const auth = firebase.auth()
+
   firebase
     .auth()
     .signInWithPopup(provider)
@@ -42,40 +45,46 @@ const creditialsWithPopup = provider => {
 export const submitOnBoardingForm = payload => {
   //return a promise to handleSubmit, dont catch the error, or handleSubmit would
   // get a resolved result instead of catching the error bc the erro already solved here..
+  const db = firebase.firestore()
+
   return db.collection("onboarding_form_submission").add(payload)
 }
 
 // create users with email and password
-export const createUser_connectForm = async (restaurantID, formSubmission, emailpwVals) => {
-
+export const createUser_connectForm = async (
+  restaurantID,
+  formSubmission,
+  emailpwVals
+) => {
   //create user with email and pw
 
   await createUserWithEmailPw(emailpwVals.email, emailpwVals.password)
 
-  const createUserWithRestaurant = firebaseFunctions.httpsCallable("create_user_with_restaurant");
+  const createUserWithRestaurant = firebaseFunctions.httpsCallable(
+    "create_user_with_restaurant"
+  )
 
   const functionData = {
     restaurantID: restaurantID,
     name: formSubmission.name,
-    phone: formSubmission.phone
-  }   
+    phone: formSubmission.phone,
+  }
 
   try {
     let result = await createUserWithRestaurant(functionData)
-    return result;
+    return result
   } catch (error) {
     console.error("within firebase service:", error)
   }
-
-
 }
 
 // create users with google account
 const createUserWithEmailPw = async (email, pw) => {
-  try{
+  try {
+    const auth = firebase.auth()
+
     await auth.createUserWithEmailAndPassword(email, pw)
-  }
-  catch(err){
+  } catch (err) {
     console.log(err)
     alert(err)
   }
@@ -83,15 +92,20 @@ const createUserWithEmailPw = async (email, pw) => {
 
 // create users with google account
 export const createUserWithGoogle = () => {
+  const googleProvider = new firebase.auth.GoogleAuthProvider()
   creditialsWithPopup(googleProvider)
 }
 // create users with fb account
 export const createUserWithFacebook = () => {
+  const facebookProvider = new firebase.auth.FacebookAuthProvider()
+
   creditialsWithPopup(facebookProvider)
 }
 
 //sign out users
 export const signOut = () => {
+  const auth = firebase.auth()
+
   auth
     .signOut()
     .then(() => alert("Sign out Succesfully!"))
@@ -99,8 +113,10 @@ export const signOut = () => {
 }
 
 // used to conditionally rendering your UI
-export const monitorAuth = (setUser) => {
+export const monitorAuth = setUser => {
+  const auth = firebase.auth()
+
   auth.onAuthStateChanged(user => {
-    user? setUser(user) : setUser(null)
+    user ? setUser(user) : setUser(null)
   })
 }
